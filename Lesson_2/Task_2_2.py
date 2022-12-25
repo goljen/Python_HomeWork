ecea8f2808f2b64ee69e30a29207faf2009e842b
GolJen <goljen@gmail.com>
2022-12-25 22:16:15 +0300

    Add Task 2.2

---
+++ Lesson_2/Task_2_2.py
@@ -0,0 +1,10 @@
+# Программа, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+number = int(input("Введите натуральное число: "))
+for count in range(0, number):
+    if  count != 0:
+        product_numbers.append(product_numbers[count - 1] * (count + 1))
+    else:
+        product_numbers = [count + 1]
+           
+print('Набор произведений чисел от 1 до %d :'%(number), product_numbers)     
