e6f79ace51ebd3a4e4a81eff40c57fc881286ace
GolJen <goljen@gmail.com>
2022-12-28 01:24:26 +0300

    Add Task 2.4

---
+++ Lesson_2/Task_2_4_*.py
@@ -0,0 +1,23 @@
+# Программа создает список из N элементов, заполненных числами из промежутка [-N, N], N вводит пользователь
+# и расчитывает произведение элементов на позициях, указанных пользователем.
+
+import random
+
+# подпрограмма создания списка
+def create_list(list_members):
+    list_original = []
+    for i in range(list_members):
+    #    list_original.append(i)
+        list_original.append(random.randint(- list_members, list_members))
+    return list_original
+
+new_list = []
+# second_list = []
+number = int(input("Введите натуральное число: "))
+new_list = create_list(number)
+print("Исходный список: ")
+print(new_list)
+first_position = int(input('Введите первое натуральное число от 0 до %d :'%(number - 1)))
+second_position = int(input('Введите второе натуральное число от 0 до %d не равное первому :'%(number - 1)))
+product_position = new_list[first_position] * new_list[second_position]
+print('Произведение элементов во введенных позициях = ', product_position)
